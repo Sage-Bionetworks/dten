c02dcac8ccf7747420bb0b83af111f9d63fce1a9
Sara JC Gosline <sara.gosline@sagebase.org>
2019-05-23 14:05:25 -0700

    fixed bug

--- R/metaviper.R
+++ R/metaviper.R
@@ -53,7 +53,7 @@ getGeneEntrezMapping<-function(genes){
     mapped_genes <- AnnotationDbi::mappedkeys(x)
       xx <- AnnotationDbi::as.list(x[mapped_genes])
     inds=match(genes,names(xx ))
-      entrez_list=data.frame(gene=genes,entrezgene=xx[inds])
+      entrez_list=data.frame(gene=genes,entrezgene=unlist(xx[inds]))
 #    mart <- useMart('ensembl',dataset='hsapiens_gene_ensembl')
 
  #   entrez_list <- getBM(filters ="hgnc_symbol",
